56bccc70a0a5deb92fd050efca1eb8936e1784e1
Luís Felipe <degaus@live.com>
2020-05-19 18:47:22 -0300

    Função para deletar instrutor

--- instructors.js
+++ instructors.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const data = require("./data.json");
-const { age , date } = require("./ultis");
+const { age, date } = require("./ultis");
 
 // SHOW
 exports.show = (request, response) => {
@@ -76,9 +76,58 @@ exports.edit = function (request, response) {
 
   const instructor = {
     ...foundInstructor,
-    birth: date(foundInstructor.birth)
-  }
-
+    birth: date(foundInstructor.birth),
+  };
 
   return response.render("instructors/edit", { instructor });
+};
+
+// PUT
+exports.put = function (request, response) {
+  const { id } = request.body;
+
+  let index = 0;
+
+  const foundInstructor = data.instructors.find(function (
+    instructor,
+    foundIndex
+  ) {
+    if (id == instructor.id) {
+      index = foundIndex;
+      return true;
+    }
+  });
+
+  if (!foundInstructor) return response.send("Instrutor não encontrado.");
+
+  const instructor = {
+    ...foundInstructor,
+    ...request.body,
+    birth: Date.parse(request.body.birth),
+  };
+
+  data.instructors[index] = instructor;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) return response.send("Temos erro!");
+  });
+
+  return response.redirect(`/instructors/${id}`);
+};
+
+// DELETE
+exports.delete = function (request, response) {
+  const { id } = request.body;
+
+  let filteredInstructors = data.instructors.filter((instructor) => {
+    return instructor.id != id;
+  });
+
+  data.instructors = filteredInstructors;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) return response.send("Temos erro!");
+
+    return response.redirect(`/instructors`);
+  });
 };
